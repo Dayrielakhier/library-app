4c4de78d40925a4d5abde705af75e9de8829559f
Dayrielakhier <176357699+Dayrielakhier@users.noreply.github.com>
2025-04-11 22:38:00 +0800

    Add event listeners to new book and submit buttons
    "New book" button will show dialog when clicked and submit button will
    add the input values to book object and library, then display.

--- javascript.js
+++ javascript.js
@@ -19,10 +19,32 @@ function display() {
     for (const book of myLibrary) {
         const row = document.createElement('tr')
         for (const property in book) {
-            const cell = document.createElement('td')
-            cell.textContent = book[property]
-            row.appendChild(cell)
+            if (property.includes("id")) continue;
+            const cell = document.createElement('td');
+            cell.textContent = book[property];
+            row.appendChild(cell);
         }
         tbody.appendChild(row)
     }
 }
+
+const showDialog = document.querySelector("#showDialog")
+const dialog = document.querySelector("dialog")
+
+showDialog.addEventListener("click", () => {
+    dialog.showModal()
+})
+
+const submit = document.querySelector('#submit')
+const title = document.querySelector("#title")
+const author = document.querySelector("#author")
+const pages = document.querySelector("#pages")
+const read = document.querySelector("#read")
+
+submit.addEventListener("click", (event) => {
+    event.preventDefault();
+    addBookToLibrary(title.value, author.value, pages.value, read.value);
+    display();
+    dialog.close();
+})
+
