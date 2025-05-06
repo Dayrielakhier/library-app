6e3951e6d17dc4ef285ab2d6ba209ce5fb0db33f
Dayrielakhier <176357699+Dayrielakhier@users.noreply.github.com>
2025-05-06 14:38:00 +0800

    Change function constructor to class

--- javascript.js
+++ javascript.js
@@ -1,11 +1,13 @@
 const myLibrary = []
 
-function Book(title, author, pages, read) {
+class Book {
+    constructor(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
     this.id = crypto.randomUUID()
+    }
 }
 
 function addBookToLibrary(title, author, pages, read) {
